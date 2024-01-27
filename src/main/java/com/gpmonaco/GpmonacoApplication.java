0bfb0a5c8f4d3bd3f8a87ff1a5533b41c8de7899
Lena Loncarevic <l.loncarevic-int@levi9.com>
2024-01-27 21:59:11 +0100

    Initialization

---
+++ src/main/java/com/gpmonaco/GpmonacoApplication.java
@@ -0,0 +1,13 @@
+package com.gpmonaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpmonacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpmonacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpmonaco/GpmonacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpmonaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpmonacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
